6d41fe90eebe736b2d13eb1d657094585ee79e62
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-01 03:16:21 +0330

    - Create ShipImpacted Property In FireShotResponse

--- BattleShip.BLL/Responses/FireShotResponse.cs
+++ BattleShip.BLL/Responses/FireShotResponse.cs
@@ -9,6 +9,7 @@ namespace BattleShip.BLL.Responses
     public class FireShotResponse
     {
         public ShotStatus ShotStatus { get; set; }
+        public string ShipImpacted { get; set; }
 
     }
 }

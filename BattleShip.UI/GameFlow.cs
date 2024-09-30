ef05b7088472e5b19c2464587b4dada2441e0ee0
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-01 02:00:21 +0330

    - Create Start Method In GameFlow Class

--- BattleShip.UI/GameFlow.cs
+++ BattleShip.UI/GameFlow.cs
@@ -14,5 +14,37 @@ namespace BattleShip.UI
         {
             gm = new GameState() { IsPlayer1 = false, Player1 = new Player(), Player2 = new Player() };
         }
+
+        public void Start()
+        {
+            GameSetup GameSetup = new GameSetup(gm);
+            GameSetup.Setup();
+
+            do
+            {
+                GameSetup.SetBoard();
+                FireShotResponse shotresponse;
+                do
+                {
+                    ControlOutput.ResetScreen(new Player[] { gm.Player1, gm.Player2 });
+                    ControlOutput.ShowWhoseTurn(gm.IsPlayer1 ? gm.Player1 : gm.Player2);
+                    ControlOutput.DrawHistory(gm.IsPlayer1 ? gm.Player2 : gm.Player1);
+                    Coordinate ShotPoint = new Coordinate(1, 1);
+                    shotresponse = Shot(gm.IsPlayer1 ? gm.Player2 : gm.Player1, gm.IsPlayer1 ? gm.Player1 : gm.Player2, out ShotPoint);
+
+                    ControlOutput.ResetScreen(new Player[] { gm.Player1, gm.Player2 });
+                    ControlOutput.ShowWhoseTurn(gm.IsPlayer1 ? gm.Player1 : gm.Player2);
+                    ControlOutput.DrawHistory(gm.IsPlayer1 ? gm.Player2 : gm.Player1);
+                    ControlOutput.ShowShotResult(shotresponse, ShotPoint, gm.IsPlayer1 ? gm.Player1.Name : gm.Player2.Name);
+                    if (shotresponse.ShotStatus != ShotStatus.Victory)
+                    {
+                        Console.WriteLine("Press any key to continue to switch to " + (gm.IsPlayer1 ? gm.Player2.Name : gm.Player1.Name));
+                        gm.IsPlayer1 = !gm.IsPlayer1;
+                        Console.ReadKey();
+                    }
+                } while (shotresponse.ShotStatus != ShotStatus.Victory);
+
+            } while (ControlInput.CheckQuit());
+        }
     }
 }

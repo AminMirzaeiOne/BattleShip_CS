e86b5e8ecfd3a4e26ec80871b659e1b854533825
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-01 02:03:29 +0330

    - Create Setup Method In GameState Class

--- BattleShip.UI/GameSetup.cs
+++ BattleShip.UI/GameSetup.cs
@@ -13,5 +13,43 @@ namespace BattleShip.UI
         {
             _gm = gm;
         }
+
+        public void Setup()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            ControlOutput.ShowFlashScreen();
+            ControlOutput.ShowHeader();
+
+            GameLevel gamelevel = GameLevel.Easy;
+            string[] userSetUp = ControlInput.GetNameFromUser();
+            switch (userSetUp[2])
+            {
+                case "h":
+                    gamelevel = GameLevel.Hard;
+                    break;
+                case "m":
+                    gamelevel = GameLevel.Medium;
+                    break;
+                default:
+                    gamelevel = GameLevel.Easy;
+                    break;
+            }
+
+            _gm.Player1.Name = userSetUp[0];
+            _gm.Player1.IsPC = false;
+            _gm.Player1.Win = 0;
+            _gm.Player1.GameLevel = gamelevel;
+
+            _gm.Player2.Name = userSetUp[1];
+            _gm.Player2.Win = 0;
+            _gm.Player2.GameLevel = gamelevel;
+
+            //vs Computer
+            if (userSetUp[1] == "")
+            {
+                _gm.Player2.Name = "Computer";
+                _gm.Player2.IsPC = true;
+            }
+        }
     }
 }

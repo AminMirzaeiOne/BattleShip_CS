60e4ad014a54fbf7377d74cebd8220fe16c36199
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-01 01:48:34 +0330

    - Create GetNumberFromLetter Function In ControlInput Class

--- BattleShip.UI/ControlInput.cs
+++ BattleShip.UI/ControlInput.cs
@@ -175,5 +175,46 @@ namespace BattleShip.UI
 
         }
 
+        static int GetNumberFromLetter(string letter)
+        {
+            int result = -1;
+            switch (letter.ToLower())
+            {
+                case "a":
+                    result = 1;
+                    break;
+                case "b":
+                    result = 2;
+                    break;
+                case "c":
+                    result = 3;
+                    break;
+                case "d":
+                    result = 4;
+                    break;
+                case "e":
+                    result = 5;
+                    break;
+                case "f":
+                    result = 6;
+                    break;
+                case "g":
+                    result = 7;
+                    break;
+                case "h":
+                    result = 8;
+                    break;
+                case "i":
+                    result = 9;
+                    break;
+                case "j":
+                    result = 10;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+
     }
 }

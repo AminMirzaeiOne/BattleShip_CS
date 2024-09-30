f6b964ccb4d67f2d0ae34a69cf2c902a99975b1c
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-01 01:52:29 +0330

    - Create ShowFlashScreen Function In ControlOutput Class

--- BattleShip.UI/ControlOutput.cs
+++ BattleShip.UI/ControlOutput.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BattleShip.UI
@@ -9,6 +10,19 @@ namespace BattleShip.UI
     public class ControlOutput
     {
         static int counttime = 0;
+        public static void ShowFlashScreen()
+        {
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("        ********************************************");
+            Console.Write("        *"); Console.ForegroundColor = ConsoleColor.White; Console.Write("             Battleship Game              "); Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*");
+            Console.WriteLine("        ********************************************");
+            Console.ForegroundColor = ConsoleColor.White;
 
+            System.Threading.Timer t = new System.Threading.Timer(ClearFlashScreen, null, 0, 1000);
+            Thread.Sleep(2100);
+            t.Dispose();
+
+        }
     }
 }

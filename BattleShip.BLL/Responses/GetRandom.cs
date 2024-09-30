e104a537ab405dc63ae174c9d195cd38aeb21321
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-01 03:19:24 +0330

    - Create WhoseFirst Function In GetRandom Class

--- BattleShip.BLL/Responses/GetRandom.cs
+++ BattleShip.BLL/Responses/GetRandom.cs
@@ -9,6 +9,9 @@ namespace BattleShip.BLL.Responses
     public class GetRandom
     {
         public static System.Random rand = new System.Random();
-
+        public static bool WhoseFirst()
+        {
+            return rand.Next(1, 10) <= 5;
+        }
     }
 }

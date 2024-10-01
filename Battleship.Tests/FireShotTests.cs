7b790c7c6e6a6946d2f448f236e6fe4eebd37015
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-01 05:13:10 +0330

    - Fixed Erros In FireShotTests Class

--- Battleship.Tests/FireShotTests.cs
+++ Battleship.Tests/FireShotTests.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BattleShip.BLL.GameLogic;
+using BattleShip.BLL.Requests;
+using BattleShip.BLL.Responses;
+using BattleShip.BLL.Ships;
 
 namespace Battleship.Tests
 {

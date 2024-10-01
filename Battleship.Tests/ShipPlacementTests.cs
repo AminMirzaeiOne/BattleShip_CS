99c6197c3704d45d9f761054e64c48e7100763b9
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-01 04:59:16 +0330

    - Create CanNotPlaceShipOffBoard Method In ShipPlacementTests Class

--- Battleship.Tests/ShipPlacementTests.cs
+++ Battleship.Tests/ShipPlacementTests.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 namespace Battleship.Tests
 {
+    [TestFixture]
     public class ShipPlacementTests
     {
+        [Test]
+        public void CanNotPlaceShipOffBoard()
+        {
+            Board board = new Board();
+            PlaceShipRequest request = new PlaceShipRequest()
+            {
+                Coordinate = new Coordinate(15, 10),
+                Direction = ShipDirection.Up,
+                ShipType = ShipType.Destroyer
+            };
 
+            var response = board.PlaceShip(request);
+
+            Assert.AreEqual(ShipPlacement.NotEnoughSpace, response);
+        }
     }
 }

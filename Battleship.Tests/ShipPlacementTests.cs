3510ca6f8386d9ee6677d87312ec2f00354f3b69
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-01 05:00:20 +0330

    - Create CanNotOverlapShips Method In ShipPlacementTests Class

--- Battleship.Tests/ShipPlacementTests.cs
+++ Battleship.Tests/ShipPlacementTests.cs
@@ -40,5 +40,36 @@ namespace Battleship.Tests
 
             Assert.AreEqual(ShipPlacement.NotEnoughSpace, response);
         }
+
+        [Test]
+        public void CanNotOverlapShips()
+        {
+            Board board = new Board();
+
+            // let's put a carrier at (10,10), (9,10), (8,10), (7,10), (6,10)
+            var carrierRequest = new PlaceShipRequest()
+            {
+                Coordinate = new Coordinate(10, 10),
+                Direction = ShipDirection.Left,
+                ShipType = ShipType.Carrier
+            };
+
+            var carrierResponse = board.PlaceShip(carrierRequest);
+
+            Assert.AreEqual(ShipPlacement.Ok, carrierResponse);
+
+            // now let's put a destroyer overlapping the y coordinate
+            var destroyerRequest = new PlaceShipRequest()
+            {
+                Coordinate = new Coordinate(9, 9),
+                Direction = ShipDirection.Down,
+                ShipType = ShipType.Destroyer
+            };
+
+            var destroyerResponse = board.PlaceShip(destroyerRequest);
+
+            Assert.AreEqual(ShipPlacement.Overlap, destroyerResponse);
+        }
+
     }
 }

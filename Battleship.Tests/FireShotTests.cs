3282a7a9c0ba3e6a8a725310378444634466bd5f
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-01 04:48:12 +0330

    - Add Summary Comment To FireShotTests Class

--- Battleship.Tests/FireShotTests.cs
+++ Battleship.Tests/FireShotTests.cs
@@ -8,5 +8,30 @@ namespace Battleship.Tests
 {
     public class FireShotTests
     {
+        #region "Board Setup"
+        /// <summary>
+        /// Let's set up a board as follows:
+        /// Destroyer: (1,8) (2,8)
+        /// Cruiser: (3,1) (3,2) (3,3)
+        /// Sub: (1,5) (2,5) (3,5)
+        /// Battleship: (10,6) (10,7) (10,8) (10, 9)
+        /// Carrier: (4,4) (5,4) (6,4) (7,4) (8,4)
+        /// 
+        ///    1 2 3 4 5 6 7 8 9 10
+        ///  1     R
+        ///  2     R
+        ///  3     R
+        ///  4       C C C C C
+        ///  5 S S S
+        ///  6                   B
+        ///  7                   B
+        ///  8 D D               B
+        ///  9                   B
+        /// 10
+        /// </summary>
+        /// <returns>A board that is ready to play</returns>
+        /// 
+
+
     }
 }
